519dfe2a8c6ea10ff2343ad222a18fb981672808
lattexi <laurikarhu@outlook.com>
2023-10-31 00:14:15 +0200

    9

---
+++ Moduuli 11/11.1.py
@@ -0,0 +1,31 @@
+class Julkaisu:
+    def __init__(self, nimi):
+        self.nimi = nimi
+
+class Kirja(Julkaisu):
+    def __init__(self, nimi, kirjoittaja, sivumaara):
+        super().__init__(nimi)
+        self.kirjoittaja = kirjoittaja
+        self.sivumaara = sivumaara
+
+    def tulosta_tiedot(self):
+        print(f"Kirjan nimi: {self.nimi}")
+        print(f"Kirjoittaja: {self.kirjoittaja}")
+        print(f"Sivumäärä: {self.sivumaara}")
+
+class Lehti(Julkaisu):
+    def __init__(self, nimi, paatoimittaja):
+        super().__init__(nimi)
+        self.paatoimittaja = paatoimittaja
+
+    def tulosta_tiedot(self):
+        print(f"Lehden nimi: {self.nimi}")
+        print(f"Päätoimittaja: {self.paatoimittaja}")
+
+
+aku_ankka = Lehti("Aku Ankka", "Aki Hyyppä")
+hytti_nro_6 = Kirja("Hytti n:o 6", "Rosa Liksom", 200)
+
+aku_ankka.tulosta_tiedot()
+print()
+hytti_nro_6.tulosta_tiedot()
